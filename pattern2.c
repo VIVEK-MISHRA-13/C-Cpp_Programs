8a5fce9d8f72c87fbbe4f26bad89420cc677038a
Vivek Mishra <vm1343396@gmail.com>
2025-09-08 21:49:45 +0530

    3rd pattern

--- pattern2.c
+++ pattern2.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-     int n=7;
+     int n=5;
      int loop1,loop3;
      int i=1;
     if(n==3){
---
+++ pattern3.c
@@ -0,0 +1,203 @@
+#include<stdio.h>
+
+int main(){
+    int n=5;
+    int loop1;
+    if(n==3){
+        loop1 = n-1;
+    }
+    else{
+        loop1 = n-2;
+    }
+
+    int i=1;
+    while(loop1--){
+        int space1 = n;
+        while (space1--)
+        {
+            printf(" ");
+        }
+
+        int space2 = loop1;
+        
+        while (space2--)
+        {
+            printf(" ");
+        }
+        
+        int star1 = i;
+        while(star1--)
+        {
+            printf("*");
+        }
+        int star2 = i-1;
+        while (star2--)
+        {
+            printf("*");
+        }
+        
+        i++;
+        
+        printf("\n");
+        
+    }
+   
+   
+    int loop2 =n-2;
+   
+
+    while (loop2--)
+    {
+        int space1 = n;
+        while (space1--)
+        {
+            printf(" ");
+        }
+        
+        int at1 = 1;
+        while (at1--)
+        {
+            printf("@");
+        }
+        
+        int space2 = n-2;
+        while (space2--)
+        {
+            printf("-");
+        }
+
+        int at2 = 1;
+        while (at2--)
+        {
+            printf("@");
+        }
+        
+        
+        printf("\n");
+        
+        
+    }
+    
+    
+    int loop3 = 1;
+    while (loop3--)
+    {
+        int star1 = n;
+        while (star1--)
+        {
+            
+            printf("*");
+        }
+        int at1 = 1;
+        while (at1--)
+        {
+            printf("@");
+        }
+
+        int space2 = n-2;
+        while (space2--)
+        {
+            printf(" ");
+        }
+
+       int at2 = 1;
+        while (at2--)
+        {
+            printf("@");
+        }
+
+         int star2 = n;
+        while (star2--)
+        {
+            
+            printf("*");
+        }
+        printf("\n");
+        
+        
+    }
+   
+    
+    int loop4;
+    if(n==3){
+        loop4 = n-2;
+    }
+    else{
+        loop4 = n-3;
+    }
+    int j=1;
+    while (loop4--)
+    {
+        int space1 = j;
+        while (space1--)
+        {
+            printf(" ");
+        }
+
+        int star1 = loop4+1;
+        while (star1--)
+        {
+            printf("*");
+        }
+
+        int star2 = loop4;
+
+
+        while (star2--)
+        {
+            printf("*");
+        }
+
+
+        int space2 = j;
+        while (space2--)
+        {
+            printf(" ");
+        }
+        
+
+        int space3 = n;
+        while (space3--)
+        {
+            printf(" ");
+        }
+
+        int space4 = j;
+        while (space4--)
+        {
+            printf(" ");
+        }
+
+
+         int star3 = loop4+1;
+        while (star3--)
+        {
+            printf("*");
+        }
+
+        int star4 = loop4;
+
+
+        while (star4--)
+        {
+            printf("*");
+        }
+
+
+        int space5 = j;
+        while (space5--)
+        {
+            printf(" ");
+        }
+        
+        
+        printf("\n");
+        
+        j++;
+        
+    }
+    
+    return 0;
+
+
+}
